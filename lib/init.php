53b15e3c802b14425af188a7ca098be3c070ca8d
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-05-08 06:20:28 +0000

    Autoload libraries, models and controllers

---
+++ lib/init.php
@@ -0,0 +1,23 @@
+<?php
+
+spl_autoload_register(function($class_name) {
+  // Get libraries paths
+  $lib_path = ROOT.DS.'lib'.DS.strtolower($class_name).'.class.php';
+  // Get controllers paths
+  $controllers_path = ROOT.DS.'app'.DS.'controllers'.DS.str_replace('controller', '_controller', strtolower($class_name)).'.php';
+  // Get models paths
+  $models_path = ROOT.DS.'app'.DS.'models'.DS.strtolower($class_name).'.php';
+
+  // Verifying the existence of the class files
+  if(file_exists($lib_path)) {
+    require_once($lib_path);
+  } elseif(file_exists($controllers_path)) {
+    require_once($controllers_path);
+  } elseif(file_exists($models_path)) {
+    require_once($models_path);
+  } else {
+    throw new \Exception("Failed to include class: ".$class_name);
+  }
+});
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "hi from index";
+// Constants required for file path localization
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__DIR__));
+
+require_once(ROOT.DS.'lib'.DS.'init.php');
+
+$post_controller = new PostsController();
+$post_model = new Post();
+
+echo ROOT;
 
 ?>
